92cfc98f0f6a224d24ce1cfad9fafa155b97d6ed
Andrew Pollack <andrewpkq@gmail.com>
2020-07-03 08:49:41 -0700

    Decomposition, updated Dockerfile config

--- webScrape/scraping.py
+++ webScrape/scraping.py
@@ -1,15 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 import re
 from datetime import date
 import json
 
 
-"""
-Class describing a reddit post.  Used for debugging and cleaning code.
-"""
 class RedditPost:
+	"""
+	Class structure defining a reddit post.
+	"""
 	def __init__(self, post_date, sub, title, link, flair,\
 				       author, comment_count, score, rank,\
 				       post_type, timestamp):
@@ -39,28 +38,54 @@ class RedditPost:
 			   "Timestamp: {}".format(self.timestamp))		
 
 
-"""
-Scrape reddit wow top posts of the day for top 25 posts.
-
-Returns: todays_top_25_posts - A list of the top 25 posts, stored as RedditPost objects. 
-"""
-def getTodaysTop25WoWPosts():
-
-	urls = ['https://old.reddit.com/r/wow/top/?sort=top&t=day']
-	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"}
+def getFormattedPost(post):
+	"""
+	Parses a given post, returning it as a formatted RedditPost object
+	"""
 	flair_regex = re.compile("(linkflair-.*)")
 	post_regex = re.compile('(thing id.*)')
 	title_regex = re.compile('(title may-blank.*)')
 	today_date = date.today().strftime("%m-%d-%Y")
 
+	post_desc = post.find("a",{'class': title_regex})
+	
+	post_author = post.get('data-author')
+	post_link = post.get('data-permalink')
+	post_comment_count = post.get('data-comments-count')
+	post_title = post_desc.text
+	post_score = post.get('data-score')
+	post_rank = post.get('data-rank')
+	post_type = post.get('data-type')
+	post_timestamp = post.get('data-timestamp')
+	post_flair = list(filter(flair_regex.match, post.get('class')))
+	if len(post_flair) == 0:
+		post_flair = ["linkflair-none"]
+	post_flair = post_flair[0][len("linkflair-"):]
+	post_date = today_date
+	post_sub = "/r/wow"
+
+	currPost = RedditPost(post_date, post_sub,\
+							post_title, post_link, post_flair,\
+							post_author, post_comment_count, post_score,\
+							post_rank, post_type, post_timestamp)
+
+	return currPost
+
+
+def getTodaysTop25WoWPosts():
+	"""
+	Scrapes reddit wow top posts of the day for top 25 posts.
+	"""
+	urls = ['https://old.reddit.com/r/wow/top/?sort=top&t=day']
+	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+			    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 \
+				Safari/537.36"}
 	todays_top_25_posts = []
 
 	for url in urls:
-		# Located by finding on dota2 wiki
 		request_text = requests.get(url, headers=headers).text
 		soup = BeautifulSoup(request_text,'lxml')
 		post_regex = re.compile('(thing id.*)')
-		title_regex = re.compile('(title may-blank.*)')
 
 		post_table = soup.find_all('div',{'class': post_regex})
 
@@ -69,29 +94,7 @@ def getTodaysTop25WoWPosts():
 				# Advertisement, skip!
 				continue
 
-			post_desc = post.find("a",{'class': title_regex})
-			
-			post_author = post.get('data-author')
-			post_link = post.get('data-permalink')
-			post_comment_count = post.get('data-comments-count')
-			post_title = post_desc.text
-			post_score = post.get('data-score')
-			post_rank = post.get('data-rank')
-			post_type = post.get('data-type')
-			post_timestamp = post.get('data-timestamp')
-			post_flair = list(filter(flair_regex.match, post.get('class')))
-			if len(post_flair) == 0:
-				post_flair = ["linkflair-none"]
-			post_flair = post_flair[0][len("linkflair-"):]
-			post_date = today_date
-			post_sub = "/r/wow"
-
-
-			currPost = RedditPost(post_date, post_sub,\
-								  post_title, post_link, post_flair,\
-							      post_author, post_comment_count, post_score,\
-							      post_rank, post_type, post_timestamp)
-
+			currPost = getFormattedPost(post)
 			todays_top_25_posts.append(currPost)
 
 	return todays_top_25_posts
@@ -114,8 +117,9 @@ def flushToDisk(post_list):
 
 	mewDict = {"posts": json_list}
 
-	with open(file_name, 'w', newline='') as fp:
-		json.dump(mewDict, fp, indent=4)
+	print(mewDict)
+	#with open(file_name, 'w', newline='') as fp:
+	#	json.dump(mewDict, fp, indent=4)
 
 
 def main():
